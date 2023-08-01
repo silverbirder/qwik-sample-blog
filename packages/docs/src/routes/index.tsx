ba2c7f3f2670bb9c6ec69e92f2410297b98b925a
shibamoto <silverbirder@gmail.com>
2023-08-01 14:49:40 +0900

    first commit

---
+++ packages/docs/src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Counter } from "@sb/ui";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi 👋</h1>
+      <p>
+        Can't wait to see what you build with qwik!
+        <br />
+        Happy coding.
+      </p>
+      <Counter />
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
