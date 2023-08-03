f3c4b63f9b49b6ec8defb3363ffde627ff3af6dc
shibamoto <silverbirder@gmail.com>
2023-08-03 14:22:11 +0900

    feat: add partytown

--- packages/docs/src/root.tsx
+++ packages/docs/src/root.tsx
@@ -7,6 +7,7 @@ import {
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
+import { QwikPartytown } from "./components/partytown/partytown";
 
 export default component$(() => {
   /**
@@ -20,6 +21,12 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <QwikPartytown forward={["dataLayer.push"]} />
+        <script
+          async
+          type="text/partytown"
+          src="https://www.googletagmanager.com/gtag/js?id=G-QV2CWHGJB6"
+        />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
