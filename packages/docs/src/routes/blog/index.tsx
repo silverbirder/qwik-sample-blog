a8273ea4661ba8d3922fa5fb326d984e911032ce
shibamoto <silverbirder@gmail.com>
2023-08-08 14:29:29 +0900

    feat: add blog tag and recommend

--- packages/docs/src/routes/blog/index.tsx
+++ packages/docs/src/routes/blog/index.tsx
@@ -1,42 +1,8 @@
-import { component$, Resource, useResource$ } from "@builder.io/qwik";
-import type { DocumentHeadProps } from "@builder.io/qwik-city";
+import { component$, useContext } from "@builder.io/qwik";
 import { PostSummaryList } from "@sb/ui";
-import { PostSummary } from "@sb/ui/lib-types/models";
-import { asyncMap } from "~/util";
-
-export const getPosts = async (): Promise<PostSummary[]> => {
-  const modules = await import.meta.glob(
-    "/src/routes/blog/contents/**/**/index.mdx"
-  );
-  const posts = await asyncMap(Object.keys(modules), async (path) => {
-    const data = (await modules[path]()) as DocumentHeadProps;
-    return {
-      title: data.head.title || "",
-      description:
-        data.head.meta.find((m) => m.name === "description")?.content || "",
-      permalink: `/blog/contents${data.head.frontmatter.permalink}`,
-      date: data.head.frontmatter.date,
-      tags: data.head.frontmatter.tags,
-      published: data.head.frontmatter.published,
-    };
-  });
-  return posts.sort((a, b) => {
-    const dateA = new Date(a.date).getTime();
-    const dateB = new Date(b.date).getTime();
-    return dateB - dateA;
-  });
-};
+import { BlogContentsContext } from "./layout";
 
 export default component$(() => {
-  const postsResource = useResource$(getPosts);
-  return (
-    <>
-      <main class="main">
-        <Resource
-          value={postsResource}
-          onResolved={(posts) => <PostSummaryList data={posts} />}
-        />
-      </main>
-    </>
-  );
+  const { posts } = useContext(BlogContentsContext);
+  return <PostSummaryList data={posts}></PostSummaryList>;
 });
